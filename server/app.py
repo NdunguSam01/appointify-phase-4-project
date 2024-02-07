40c631e9d63e3df34bfcd814ef87e82b64c40e68
Samuel Muigai <ndungusam01@gmail.com>
2024-02-07 11:04:47 +0300

    Added validation functions to the doctors and patient models and created the login rescource

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, request, session, make_response
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from models import db, Admin, Patient, Doctor, Appointment
+import hashlib
 
 app=Flask(__name__)
 
@@ -18,9 +19,26 @@ db.init_app(app)
 
 class Index(Resource):
     def get(self):
-        return "<h1>Main Page</h1>"
+        return "Main Page"
     
 api.add_resource(Index, "/")
+
+class AdminLogin(Resource):
+    def post(self):
+        email=request.json["email"]
+        password=request.json["password"]
+
+        admin=Admin.query.filter(Admin.email==email).first()
+
+        if not admin:
+            return make_response(jsonify({"error": "No acount exists with the given email"}),401)
+        
+        if admin.password != hashlib.md5(password.encode("utf-8")).hexdigest():
+            return make_response(jsonify("Incorrect password"), 401)
+
+        return make_response(jsonify("Login successful"), 200)
+
+api.add_resource(AdminLogin, "/login")
 
 
 if __name__ == "__main__":
