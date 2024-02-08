aa3e6057722a6b52645e3329c72fb878de0f8896
Samuel Muigai <ndungusam01@gmail.com>
2024-02-08 15:27:11 +0300

    Done with the patients section

--- client/src/Components/Patients/Patient Registration Form.js
+++ client/src/Components/Patients/Patient Registration Form.js
@@ -3,166 +3,148 @@ import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Stack from 'react-bootstrap/Stack';
 import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css';
-const PatientRegistrationForm = () => 
+import { toast, ToastContainer } from 'react-toastify';
+const PatientRegistrationForm = ({patientData, setPatientData}) => 
 {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [dob, setDob] = useState("");
-  const [gender, setGender] = useState("");
-  const [address, setAddress] = useState("");
-  const [bloodGroup, setBloodGroup] = useState("");
-
   const [patientFormData, setPatientFormData]=useState(
     {
       first_name: '',
       last_name: '',
       email: " ",
-      phone: '',
+      phone_number: '',
       gender: '',
       dob: '',
       address: '' ,
       blood_group: ''
     }
   )
-  // const handleNameChange = (e) => {
-  //   setName(e.target.value);
-  // };
 
-  // const handleEmailChange = (e) => {
-  //   setEmail(e.target.value);
-  // };
-
-  // const handlePhoneChange = (e) => {
-  //   setPhone(e.target.value);
-  // };
-
-  // const handleDobChange = (e) => {
-  //   setDob(e.target.value);
-  // };
-
-  // const handleGenderChange = (e) => {
-  //   setGender(e.target.value);
-  // };
-
-  // const handleAddressChange = (e) => {
-  //   setAddress(e.target.value);
-  // };
-
-  // const handleBloodGroupChange = (e) => {
-  //   setBloodGroup(e.target.value);
-  // };
-  // const [phone, setPhoneNumber] = useState('');
-
-  const handlePhoneNumberChange = (value) => {
-    setPhone(value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Patient Registration Form Submitted:", {
-      name,
-      email,
-      phone,
-      dob,
-      gender,
-      address,
-      bloodGroup,
-    });
-  };
-
-  return(
-    <Form onSubmit={handleSubmit}>
-      <Row className="mb-3">
-        <Form.Group as={Col}>
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="name" placeholder="First Name" id="first_name" required></Form.Control>
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="name" placeholder="Last Name" id="last_name" required></Form.Control>
-        </Form.Group>
-      </Row>
-      <Form.Group className="mb-3">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="City" type="text" id="address" required></Form.Control>
-      </Form.Group>
-      <Row className="mb-3">
-        <Form.Group as={Col}>
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Email address" id="email" required></Form.Control>
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Label>Phone Number</Form.Label>
-          <PhoneInput international defaultCountry="KE" value={phone} onChange={handlePhoneNumberChange} id="phone" required></PhoneInput>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col}>
-          <Form.Label>Date of birth</Form.Label>
-          <Form.Control type="date" placeholder="Date of birth" id="dob" required></Form.Control>
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Label>Gender</Form.Label>
-          <Form.Select aria-label="Select gender">
-            <option value="">Select gender</option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-          </Form.Select>
-        </Form.Group>
-      </Row>
-    </Form>      
+  const handleChange= e => setPatientFormData(
+    {
+      ...patientFormData,
+      [e.target.id]: e.target.value
+    }
   )
-  // return (
-  //   <form onSubmit={handleSubmit}>
-  //     <h2>PATIENT REGISTRATION FORM</h2>
-  //     <div>
-  //       <label htmlFor="name">Name:</label>
-  //       <input type="text" id="name" value={name} onChange={handleNameChange} />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="email">Email:</label>
-  //       <input type="email" id="email" value={email} onChange={handleEmailChange} />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="phone">Phone:</label>
-  //       <input type="tel" id="phone" value={phone} onChange={handlePhoneChange} />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="dob">Date of Birth:</label>
-  //       <input type="date" id="dob" value={dob} onChange={handleDobChange} />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="gender">Gender:</label>
-  //       <select id="gender" value={gender} onChange={handleGenderChange}>
-  //         <option value="">Select</option>
-  //         <option value="male">Male</option>
-  //         <option value="female">Female</option>
-  //       </select>
-  //     </div>
-  //     <div>
-  //       <label htmlFor="address">Address:</label>
-  //       <input type="text" id="address" value={address} onChange={handleAddressChange} />
-  //     </div>
-  //     <div>
-  //       <label htmlFor="bloodGroup">Blood Group:</label>
-  //       <select id="bloodGroup" value={bloodGroup} onChange={handleBloodGroupChange}>
-  //         <option value="">Select</option>
-  //         <option value="a+">A+</option>
-  //         <option value="a-">A-</option>
-  //         <option value="b+">B+</option>
-  //         <option value="b-">B-</option>
-  //         <option value="ab+">AB+</option>
-  //         <option value="ab-">AB-</option>
-  //         <option value="o+">O+</option>
-  //         <option value="o-">O-</option>
-  //       </select>
-  //     </div>
-  //     <button type="submit">Submit</button>
-  //   </form>
-  // );
+
+  const handlePhoneNumberChange = (value) => 
+  {
+    setPatientFormData(
+      {
+        ...patientFormData,
+        phone_number : value
+      })
+  };
+
+  const handleSubmit = (e) => 
+  {
+    e.preventDefault();  
+    fetch("/patients",
+    {
+      method: "POST",
+      headers:
+      {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(patientFormData)
+    })
+    .then(response =>
+      {
+        if(response.ok)
+        {
+          response.json()
+          .then(patient => 
+            {
+              setPatientData([...patientData, patient])
+              toast.success("User added successfully",
+              {
+                position : 'top-right'
+              })
+            })
+        }
+        else
+        {
+          response.json()
+          .then(error => console.log(error))
+        }
+      })
+      .then(setPatientFormData(
+        {
+          first_name: '',
+          last_name: '',
+          email: " ",
+          phone_number: '',
+          gender: '',
+          dob: '',
+          address: '' ,
+          blood_group: ''
+        }
+      ))
+  };
+  return(
+    <>
+    <ToastContainer autoClose={2000} className="mx-1" pauseOnHover={false}/>
+      <Form onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col}>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="name" placeholder="First Name" id="first_name" value={patientFormData.first_name} onChange={handleChange} required></Form.Control>
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="name" placeholder="Last Name" id="last_name" value={patientFormData.last_name} onChange={handleChange} required></Form.Control>
+          </Form.Group>
+        </Row>
+        <Form.Group className="mb-3">
+          <Form.Label>Address</Form.Label>
+          <Form.Control placeholder="City" type="text" id="address" value={patientFormData.address} onChange={handleChange} required></Form.Control>
+        </Form.Group>
+        <Row className="mb-3">
+          <Form.Group as={Col}>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Email address" id="email" value={patientFormData.email} onChange={handleChange} required></Form.Control>
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>Phone Number</Form.Label>
+            <PhoneInput international defaultCountry="KE" value={patientFormData.phone_number} onChange={handlePhoneNumberChange} required></PhoneInput>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col}>
+            <Form.Label>Date of birth</Form.Label>
+            <Form.Control type="date" placeholder="Date of birth" id="dob" value={patientFormData.dob} onChange={handleChange} required></Form.Control>
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>Gender</Form.Label>
+            <Form.Select aria-label="Select gender" value={patientFormData.gender} onChange={handleChange} id="gender">
+              <option value="">Select gender</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Label>Blood group</Form.Label>
+            <Form.Select aria-label="Select the blood group" value={patientFormData.blood_group} onChange={handleChange} id="blood_group">
+              <option value="">Select the blood group</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </Form.Select>
+          </Form.Group>
+        </Row>
+        <Stack gap={2} className="col-md-5 mx-auto">
+          <Button type="submit" variant="dark">Add new patient</Button>
+        </Stack>
+      </Form>    
+    </>  
+  )
 };
 
 export default PatientRegistrationForm;
