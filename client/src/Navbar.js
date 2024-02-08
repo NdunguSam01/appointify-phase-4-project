f3bc318e8313919dde5be6f51c58dbfad44434c6
hazel <muthoninjoki2003@gamil.com>
2024-02-08 14:44:07 +0300

    Few changes to the navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link to="/" className="navbar-brand">Hospital Dashboard</Link>
+    <Link to="/" className="navbar-brand">Dashboard</Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
