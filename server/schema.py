698f47b9569410ce1d4d18fd9c6ce1e02b553ca8
Samuel Muigai <ndungusam01@gmail.com>
2024-02-08 12:53:52 +0300

    Added a schema file

---
+++ server/schema.py
@@ -0,0 +1,22 @@
+from flask_marshmallow import Schema
+
+class AdminSchema(Schema):
+    class Meta:
+        fields=('first_name', 'last_name', 'email')
+
+admins_schema=AdminSchema(many=True)
+admin_schema=AdminSchema()
+
+class PatientSchema(Schema):
+    class Meta:
+        fields=("id","first_name","last_name","email","phone","age","gender")
+
+patient_schema=PatientSchema()
+patients_schema=PatientSchema(many=True)
+
+class DoctorSchema(Schema):
+    class Meta:
+        fields=("id", "first_name", "last_name", "age", "gender", "department", "experience")
+
+doctors_schema=DoctorSchema(many=True)
+doctor_scema=DoctorSchema()
