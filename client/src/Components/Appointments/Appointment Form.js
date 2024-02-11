ea45b751c6d35c2ae69c35a00fca10af5f18ed11
Samuel Muigai <ndungusam01@gmail.com>
2024-02-11 12:21:50 +0300

    Fixed the resetting of the appointment form values

--- client/src/Components/Appointments/Appointment Form.js
+++ client/src/Components/Appointments/Appointment Form.js
@@ -71,7 +71,7 @@ const AppointmentsForm = ({appointments, patients, doctors, setAppointments}) =>
                 <Row>
                     <Form.Group className="col-md-6 mb-3">
                         <Form.Label>Patient</Form.Label>
-                        <Form.Select aria-label="Select patient" id="patient_id" onChange={handleInputChange} required>
+                        <Form.Select aria-label="Select patient" id="patient_id" onChange={handleInputChange} required value={appointmentData.patient_id}>
                             <option value="">Please select a patient</option>
                             {patients.map(patient =>
                                 {
@@ -81,7 +81,7 @@ const AppointmentsForm = ({appointments, patients, doctors, setAppointments}) =>
                     </Form.Group>
                     <Form.Group className="col-md-6 mb-3">
                         <Form.Label>Doctor</Form.Label>
-                        <Form.Select aria-label="Select doctor" id="doctor_id" onChange={handleInputChange} required>
+                        <Form.Select aria-label="Select doctor" id="doctor_id" onChange={handleInputChange} value={appointmentData.doctor_id} required>
                             <option value="">Please select a doctor</option>
                             {doctors.map(doctor =>
                                 {
@@ -91,15 +91,15 @@ const AppointmentsForm = ({appointments, patients, doctors, setAppointments}) =>
                     </Form.Group>
                     <Form.Group className="col-md-6 mb-3">
                         <Form.Label>Date</Form.Label>
-                        <Form.Control type="date" id="date" required onChange={handleInputChange}></Form.Control>
+                        <Form.Control type="date" id="date" value={appointmentData.date} required onChange={handleInputChange}></Form.Control>
                     </Form.Group>
                     <Form.Group className="col-md-6 mb-3">
                         <Form.Label>Time</Form.Label>
-                        <Form.Control type="time" id="time" required onChange={handleInputChange}></Form.Control>
+                        <Form.Control type="time" id="time" value={appointmentData.time} required onChange={handleInputChange}></Form.Control>
                     </Form.Group>
                     <Form.Group className="col-md-12 mb-3">
                         <Form.Label>Purpose</Form.Label>
-                        <Form.Control type="textarea" id="purpose" style={{ height: '50px' }} onChange={handleInputChange} required></Form.Control>
+                        <Form.Control type="textarea" id="purpose" value={appointmentData.purpose} style={{ height: '50px' }} onChange={handleInputChange} required></Form.Control>
                     </Form.Group>
                     <Button type="submit" variant="dark" className="col-md-3 mx-auto mt-3">Create new appointment</Button>
                 </Row>
