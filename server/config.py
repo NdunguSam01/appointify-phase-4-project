da8ee1d453c64c3bdb2bad4e857016976552f0b5
Samuel Muigai <ndungusam01@gmail.com>
2024-02-14 19:41:04 +0300

    Added the redis module to the requirements file

--- server/config.py
+++ server/config.py
@@ -10,7 +10,7 @@ class ApplicationConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS= False
 
-    SESSION_TYPE = "redis"
+    # SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     # SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
