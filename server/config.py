edee5c377cbc9ddafc5711b7bf0e9cc5309b7c8d
Samuel Muigai <ndungusam01@gmail.com>
2024-02-14 19:37:38 +0300

    Added the redis module to the requirements file

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-import redis
+# import redis
 
 load_dotenv()
 
@@ -13,4 +13,4 @@ class ApplicationConfig:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    # SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
